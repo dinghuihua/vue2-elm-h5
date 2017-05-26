4cb1452919ab59e41a9604256d6d513b8a27ee86
motif <dinghuihua@evermarker.net>
2017-05-26 18:32:00 +0800

    add 首页的 header 部分

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,8 +1,35 @@
 <style lang="less" src="./home.less" scoped></style>
 <template>
   <div class="page-home" v-if="showThePage">
-    <h2>home页的内容</h2>
-
+    <header class="header">
+      <div class="location">
+        <i class="icon i-location"></i>
+        <span>北京市通州区物资学院</span>
+      </div>
+      <!-- 搜索 -->
+      <router-link to="/search">
+        <input type="text" class="search-input" placeholder="搜索商家、商品">
+      </router-link>
+      <!-- 热搜词 -->
+      <div class="hot-word">
+        <span>炸鸡</span>
+        <span>汉堡</span>
+        <span>炸鸡</span>
+        <span>麦当劳</span>
+        <span>肯德基</span>
+        <span>水饺</span>
+        <span>粽子</span>
+        <span>早餐</span>
+        <span>水煮鱼</span>
+        <span>麻辣烫</span>
+        <span>水饺</span>
+        <span>粽子</span>
+        <span>早餐</span>
+        <span>水煮鱼</span>
+        <span>麻辣烫</span>
+      </div>
+    </header>
+    
     <!-- 底部的固定导航栏 -->
     <Footer-nav></Footer-nav>
   </div>
@@ -21,7 +48,7 @@ export default {
   },
   mounted () {
     this.$store.dispatch('setLoading', true)
-    let time = Math.floor(Math.random() * 2000) // 模拟请求等待需要的时间
+    let time = Math.floor(Math.random() * 1000) // 模拟请求等待需要的时间
     setTimeout(() => {
       this.$store.dispatch('setLoading', false) // loading 隐藏
       this.showThePage = true
