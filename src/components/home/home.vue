c1aaa815cbdd595dbc614eeb41b443f4961d25f3
motif <dinghuihua@evermarker.net>
2017-05-27 13:26:54 +0800

    完成首页的滚动导航

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,4 +1,3 @@
-<style lang="less" src="./home.less" scoped></style>
 <template>
   <div class="page-home" v-if="showThePage">
     <header class="header">
@@ -17,7 +16,111 @@
         </router-link>
       </div>
     </header>
-    
+    <!-- 首页 slider 导航 -->
+    <div class="index-slider">
+      <Swipe :auto="0"> <!-- swipe 设置不自动滚动 -->
+        <Swipe-item>
+          <router-link to="/search/美食">
+            <div class="slide-item">
+              <img src="/static/images/slider-pic/slider-pic1.jpeg" alt="美食">
+              <span class="title">美食</span>
+            </div>
+          </router-link>
+          <router-link to="/search/甜品饮品">
+            <div class="slide-item">
+              <img src="/static/images/slider-pic/slider-pic2.jpeg" alt="甜品饮品">
+              <span class="title">甜品饮品</span>
+            </div>
+          </router-link>
+          <router-link to="/search/商店超市">
+            <div class="slide-item">
+              <img src="/static/images/slider-pic/slider-pic3.jpeg" alt="商店超市">
+              <span class="title">商店超市</span>
+            </div>
+          </router-link>
+          <router-link to="/search/预定早餐">
+            <div class="slide-item">
+              <img src="/static/images/slider-pic/slider-pic4.jpeg" alt="预定早餐">
+              <span class="title">预定早餐</span>
+            </div>
+          </router-link>
+          <router-link to="/search/果蔬生鲜">
+            <div class="slide-item">
+              <img src="/static/images/slider-pic/slider-pic5.jpeg" alt="果蔬生鲜">
+              <span class="title">果蔬生鲜</span>
+            </div>
+          </router-link>
+          <router-link to="/search/新店特惠">
+            <div class="slide-item">
+              <img src="/static/images/slider-pic/slider-pic6.jpeg" alt="新店特惠">
+              <span class="title">新店特惠</span>
+            </div>
+          </router-link>
+          <router-link to="/search/准时达">
+            <div class="slide-item">
+              <img src="/static/images/slider-pic/slider-pic7.jpeg" alt="准时达">
+              <span class="title">准时达</span>
+            </div>
+          </router-link>
+          <router-link to="/search/夜宵">
+            <div class="slide-item">
+              <img src="/static/images/slider-pic/slider-pic8.jpeg" alt="夜宵">
+              <span class="title">夜宵</span>
+            </div>
+          </router-link>
+        </Swipe-item>
+        <Swipe-item>
+          <router-link to="/search/土豪推荐">
+            <div class="slide-item">
+              <img src="/static/images/slider-pic/slider-pic9.jpeg" alt="土豪推荐">
+              <span class="title">土豪推荐</span>
+            </div>
+          </router-link>
+          <router-link to="/search/鲜花蛋糕">
+            <div class="slide-item">
+              <img src="/static/images/slider-pic/slider-pic10.jpeg" alt="鲜花蛋糕">
+              <span class="title">鲜花蛋糕</span>
+            </div>
+          </router-link>
+          <router-link to="/search/汉堡">
+            <div class="slide-item">
+              <img src="/static/images/slider-pic/slider-pic11.jpeg" alt="汉堡">
+              <span class="title">汉堡</span>
+            </div>
+          </router-link>
+          <router-link to="/search/日韩料理">
+            <div class="slide-item">
+              <img src="/static/images/slider-pic/slider-pic12.jpeg" alt="日韩料理">
+              <span class="title">日韩料理</span>
+            </div>
+          </router-link>
+          <router-link to="/search/麻辣烫">
+            <div class="slide-item">
+              <img src="/static/images/slider-pic/slider-pic13.jpeg" alt="麻辣烫">
+              <span class="title">麻辣烫</span>
+            </div>
+          </router-link>
+          <router-link to="/search/披萨意面">
+            <div class="slide-item">
+              <img src="/static/images/slider-pic/slider-pic14.jpeg" alt="披萨意面">
+              <span class="title">披萨意面</span>
+            </div>
+          </router-link>
+          <router-link to="/search/川湘菜">
+            <div class="slide-item">
+              <img src="/static/images/slider-pic/slider-pic15.jpeg" alt="川湘菜">
+              <span class="title">川湘菜</span>
+            </div>
+          </router-link>
+          <router-link to="/search/包子粥店">
+            <div class="slide-item">
+              <img src="/static/images/slider-pic/slider-pic16.jpeg" alt="包子粥店">
+              <span class="title">包子粥店</span>
+            </div>
+          </router-link>
+        </Swipe-item>
+      </Swipe>
+    </div>
     <!-- 底部的固定导航栏 -->
     <Footer-nav></Footer-nav>
   </div>
@@ -25,6 +128,7 @@
 
 <script>
 import { mapGetters } from 'vuex'
+import { Swipe, SwipeItem } from 'vue-swipe'
 import FooterNav from '../common/footer_nav/footer_nav'
 import axios from 'axios'
 
@@ -62,7 +166,13 @@ export default {
     }
   },
   components: {
-    FooterNav
+    FooterNav, Swipe, SwipeItem
   }
 }
 </script>
+
+<style lang="less" src="./home.less" scoped></style>
+<!-- swipe的css + 改写 -->
+<style>
+  .mint-swipe,.mint-swipe-items-wrap{overflow:hidden;position:relative;height:100%}.mint-swipe-items-wrap{-webkit-transform:translateZ(0);transform:translateZ(0)}.mint-swipe-items-wrap>div{position:absolute;-webkit-transform:translateX(-100%);transform:translateX(-100%);width:100%;height:100%;display:none}.mint-swipe-items-wrap>div.is-active{display:block;-webkit-transform:none;transform:none}.mint-swipe-indicators{position:absolute;bottom:10px;left:50%;-webkit-transform:translateX(-50%);transform:translateX(-50%)}.mint-swipe-indicator{width:8px;height:8px;display:inline-block;border-radius:100%;background:#ccc;margin:0 3px}.mint-swipe-indicator.is-active{background: #0096ff;}
+</style>
