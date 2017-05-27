4f0128577489bfd637410d4fc7cb39c17754f711
motif <dinghuihua@evermarker.net>
2017-05-27 11:46:29 +0800

    完成 请求‘热搜词’数据，填入dom

--- README.md
+++ README.md
@@ -32,7 +32,68 @@ npm install vuex --save
 ## 技术栈
 vue2 + vuex2 + vue-router2 + axios + webpack + ES6 + less + flex
 
-## 说明
+## 部分技术说明
+#### 一、 axios 的使用
+
+我们可以借助node mock数据，具体如下，在dev-server.js文件中
+
+```
+var app = express()
+
+/* start 模拟接口 */
+var appData = require('../server/data.json')
+var hotWords = appData.hot_search_word
+
+var apiRoutes = express.Router()
+
+apiRoutes.get('/getHotWords', function (req, res) {
+  res.json({
+    errno: 0,
+    data: hotWords
+  });
+})
+
+app.use('/api', apiRoutes)
+/* end 模拟接口 */
+```
+* 这样我们启动本地服务时，访问 http://localhost:8888/api/getHotWords 即可得到json数据【 ps:代码中require的是我们在本地模拟数据的一个json文件 】
+
+* 
+接口写好了，我们看可以使用它了，在这里我们用axios来发送http请求，先安装axios（也可以使用vue-resource，但自从vue2出来后，Vue.js的作者尤雨溪微博说将不维护它了，推荐用axios了）
+```
+npm install axios --save
+```
+
+具体使用举例：请求搜索热词列表, 部分js代码如下：
+
+```
+import axios from 'axios'
+const ERR_OK = 0
+axios.get('/api/getHotWords').then(response => {
+	if (response.data.errno === ERR_OK) {
+	  this.hotWords = response.data.data
+	}
+})
+```
+html代码如
+
+```
+<!-- 热搜词 -->
+<div class="hot-word">
+	<router-link class="search-word" v-for="item in hotWords" :to="'/search/' + item.search_word">
+	  <span>{{ item.title }}</span>
+	</router-link>
+</div>
+```
+#### 二、 轮播使用vue-swipe
+```
+npm install vue-swipe --save
+```
+github地址：  https://github.com/ElemeFE/vue-swipe
+
+
+
+
 
 >  如果对您有帮助，您可以点右上角 "Star" 支持一下 谢谢！ ^_^
 
--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -12,21 +12,9 @@
       </router-link>
       <!-- 热搜词 -->
       <div class="hot-word">
-        <span>炸鸡</span>
-        <span>汉堡</span>
-        <span>炸鸡</span>
-        <span>麦当劳</span>
-        <span>肯德基</span>
-        <span>水饺</span>
-        <span>粽子</span>
-        <span>早餐</span>
-        <span>水煮鱼</span>
-        <span>麻辣烫</span>
-        <span>水饺</span>
-        <span>粽子</span>
-        <span>早餐</span>
-        <span>水煮鱼</span>
-        <span>麻辣烫</span>
+        <router-link class="search-word" v-for="item in hotWords" :to="'/search/' + item.search_word" :key="item.search_word">
+          <span>{{ item.title }}</span>
+        </router-link>
       </div>
     </header>
     
@@ -38,12 +26,16 @@
 <script>
 import { mapGetters } from 'vuex'
 import FooterNav from '../common/footer_nav/footer_nav'
+import axios from 'axios'
+
+const ERR_OK = 0
 
 export default {
   name: 'home',
   data () {
     return {
-      showThePage: false // 是否展示当前页面
+      showThePage: false, // 是否展示当前页面
+      hotWords: []
     }
   },
   mounted () {
@@ -53,6 +45,7 @@ export default {
       this.$store.dispatch('setLoading', false) // loading 隐藏
       this.showThePage = true
     }, time)
+    this.getHotWords()
   },
   computed: {
     ...mapGetters([
@@ -60,7 +53,13 @@ export default {
     ])
   },
   methods: {
-
+    getHotWords () {
+      axios.get('/api/getHotWords').then(response => {
+        if (response.data.errno === ERR_OK) {
+          this.hotWords = response.data.data
+        }
+      })
+    }
   },
   components: {
     FooterNav
